9af481bdb915174f198ee09d5d51e1b7dab0df77
jhonnyrave21 <jhonnyrave21@gmail.com>
2020-12-06 23:19:52 -0500

    main

--- red-bicicletas/public/javascripts/maps.js
+++ red-bicicletas/public/javascripts/maps.js
@@ -1,11 +1,20 @@
 
  // initialize the map
- var map = L.map('mapid').setView([42.35, -71.08], 13);
+ var map = L.map('mapid').setView([-34.6012424, -58.3861497], 13);
 
  // load a tile layer
- L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
+ L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
    {
-     attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-     maxZoom: 17,
-     minZoom: 9
+     attribution: 'Tiles by <a href="http://www.openstreetmap.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>'
    }).addTo(map);
+
+   $.ajax({
+     dataType: "json",
+     url: "api/bicicletas",
+     success: function(result){
+       console.log(result);
+       result.bicicletas.forEach(function (bici) {
+         L.marker(bici.ubicacion,{title: bici.id}).addTo(map);
+       });
+     }
+   })
